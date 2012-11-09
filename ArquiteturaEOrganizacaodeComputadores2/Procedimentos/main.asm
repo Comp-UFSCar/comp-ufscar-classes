dadc68fab9170695f7fbadd662e943ed7851bdc5
Lucas Oliveira David <lucasolivdavid@gmail.com>
2012-11-08 23:30:06 -0300

    Procedimentos e funções!
    Projeto exemplo para inserção de protótipos e procedimentos separados do
    main.asm.

---
+++ ArquiteturaEOrganizacaodeComputadores2/Procedimentos/main.asm
@@ -0,0 +1,15 @@
+TITLE Add and Subtract              (AddSub.asm)
+
+; This program adds and subtracts 32-bit integers.
+; Last update: 06/01/2006
+
+include Inclusoes/AProto.inc
+
+.code
+main PROC
+
+	
+
+	exit
+main ENDP
+END main
